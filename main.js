cc16d4d2b3ba0aa099cdcca27f008ed1977d297a
Igor Bezhinarov <igorbezhinarov@yandex.ru>
2024-09-14 17:01:15 +0700

    added basic adaptive for header / added jquery

--- main.js
+++ main.js
@@ -2,3 +2,20 @@ import { Fancybox } from "@fancyapps/ui";
 
 import './sass/_style.scss';
 import "@fancyapps/ui/dist/fancybox/fancybox.css";
+
+//toggle search
+$(function() {
+    if (window.innerWidth < 992) {
+        $('.header__search_trigger').on('click', () => {
+            console.log('gay')
+            $('#title-search').slideToggle(100)
+        })
+
+        $(document).mouseup(function (e) {
+            if ($('#title-search').has(e.target).length === 0){
+                $('#title-search').slideUp(10);
+            }
+        });
+    }
+})
+
