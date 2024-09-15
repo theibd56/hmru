28d59b03a4ccde5757d3defd52f5a64752f407bd
Igor Bezhinarov <igorbezhinarov@yandex.ru>
2024-09-15 19:26:08 +0700

    update config

--- vite.config.js
+++ vite.config.js
@@ -7,9 +7,6 @@ export default defineConfig({
         rollupOptions: {
             input: {
                 main: resolve(__dirname, 'index.html'),
-                catalog: resolve(__dirname, 'catalog.html'),
-                category: resolve(__dirname, 'category.html'),
-                product: resolve(__dirname, 'product-show.html'),
             },
         },
     },
